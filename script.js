d8c1fed36186383d64ed15fa62ca5cef2d628813
bdsagnes <bdsagnes@gmail.com>
2022-07-04 10:26:38 -0300

    Dark mode

--- script.js
+++ script.js
@@ -42,3 +42,32 @@ class MobileNavbar {
     ".nav-list li",
   );
   mobileNavbar.init();
+
+  const changeThemeBtn = document.querySelector("#change-theme");
+toggleDarkMode();
+
+// toggle dark mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+
+}
+
+//  load light or dark mode
+function loadTheme() {
+    const darkMode = localStorage.getitem("dark")
+
+    if (darkMode) {
+        toggleDarkMode();
+    }
+}
+
+changeThemeBtn.addEventListener("change", function() {
+    toggleDarkMode();
+
+    //save or remove dark mode
+        localStorage.removeItem("dark");
+
+        if(document.body.classList.contains("dark")) {
+            localStorage.setItem("dark", 1);
+        }
+});
